892313f51e1d0fc5a63aee2b97c470127a3e2ac3
James Moughon <james.moughon@praxent.com>
2022-04-20 00:22:33 -0500

    CDK init

---
+++ test/growth-day-2022-aws-james-moughon.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GrowthDay2022AwsJamesMoughon from '../lib/growth-day-2022-aws-james-moughon-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/growth-day-2022-aws-james-moughon-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GrowthDay2022AwsJamesMoughon.GrowthDay2022AwsJamesMoughonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
