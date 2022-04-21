47b0e6d2b021c0c81e57fc3282118cb5280edd96
James Moughon <james.moughon@praxent.com>
2022-04-21 00:17:45 -0500

    update deploy

--- lib/growth-day-2022-aws-james-moughon-stack.ts
+++ lib/growth-day-2022-aws-james-moughon-stack.ts
@@ -1,61 +1,9 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as cdk from "aws-cdk-lib";
-import * as iam from "aws-cdk-lib/aws-iam";
 
 export class GrowthDay2022AwsJamesMoughonStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    /**
-     * Create an Identity provider for GitHub inside your AWS Account. This
-     * allows GitHub to present itself to AWS IAM and assume a role.
-     */
-    const provider = new iam.OpenIdConnectProvider(this, "MyProvider", {
-      url: "https://token.actions.githubusercontent.com",
-      clientIds: ["sts.amazonaws.com"],
-    });
-    const githubOrganisation = "jmoughon";
-    // Change this to the repo you want to push code from
-    const repoName = "growth-day-2022-aws";
-    /**
-     * Create a principal for the OpenID; which can allow it to assume
-     * deployment roles.
-     */
-    const GitHubPrincipal = new iam.OpenIdConnectPrincipal(
-      provider
-    ).withConditions({
-      StringLike: {
-        "token.actions.githubusercontent.com:sub": `repo:${githubOrganisation}/${repoName}:*`,
-      },
-    });
-
-    /**
-     * Create a deployment role that has short lived credentials. The only
-     * principal that can assume this role is the GitHub Open ID provider.
-     *
-     * This role is granted authority to assume aws cdk roles; which are created
-     * by the aws cdk v2.
-     */
-    new iam.Role(this, "GitHubActionsRole", {
-      assumedBy: GitHubPrincipal,
-      description:
-        "Role assumed by GitHubPrincipal for deploying from CI using aws cdk",
-      roleName: "github-ci-role",
-      maxSessionDuration: cdk.Duration.hours(1),
-      inlinePolicies: {
-        CdkDeploymentPolicy: new iam.PolicyDocument({
-          assignSids: true,
-          statements: [
-            new iam.PolicyStatement({
-              effect: iam.Effect.ALLOW,
-              actions: ["sts:AssumeRole"],
-              resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
-            }),
-          ],
-        }),
-      },
-    });
-
     new ec2.Vpc(this, "GrowthDay2022AwsJamesMoughonVpc", {
       // CHANGE: this is where we define how many AZs to use
       maxAzs: 2,
