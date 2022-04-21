daf520dc122138fbca45b6d38d8c02d84c4b1e93
James Moughon <james.moughon@praxent.com>
2022-04-20 23:39:30 -0500

    Test deploy

--- bin/growth-day-2022-aws-james-moughon.ts
+++ bin/growth-day-2022-aws-james-moughon.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { GrowthDay2022AwsJamesMoughonStack } from '../lib/growth-day-2022-aws-james-moughon-stack';
+import * as cdk from "aws-cdk-lib";
+import { GrowthDay2022AwsJamesMoughonStack } from "../lib/growth-day-2022-aws-james-moughon-stack";
 
 const app = new cdk.App();
-new GrowthDay2022AwsJamesMoughonStack(app, 'GrowthDay2022AwsJamesMoughonStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new GrowthDay2022AwsJamesMoughonStack(
+  app,
+  "GrowthDay2022AwsJamesMoughonStack",
+  {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    env: { account: "184526972864", region: "us-east-2" },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  }
+);
--- lib/growth-day-2022-aws-james-moughon-stack.ts
+++ lib/growth-day-2022-aws-james-moughon-stack.ts
@@ -1,16 +1,79 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
 
-export class GrowthDay2022AwsJamesMoughonStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class GrowthDay2022AwsJamesMoughonStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /**
+     * Create an Identity provider for GitHub inside your AWS Account. This
+     * allows GitHub to present itself to AWS IAM and assume a role.
+     */
+    const provider = new iam.OpenIdConnectProvider(this, "MyProvider", {
+      url: "https://token.actions.githubusercontent.com",
+      clientIds: ["sts.amazonaws.com"],
+    });
+    const githubOrganisation = "jmoughon";
+    // Change this to the repo you want to push code from
+    const repoName = "growth-day-2022-aws";
+    /**
+     * Create a principal for the OpenID; which can allow it to assume
+     * deployment roles.
+     */
+    const GitHubPrincipal = new iam.OpenIdConnectPrincipal(
+      provider
+    ).withConditions({
+      StringLike: {
+        "token.actions.githubusercontent.com:sub": `repo:${githubOrganisation}/${repoName}:*`,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GrowthDay2022AwsJamesMoughonQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /**
+     * Create a deployment role that has short lived credentials. The only
+     * principal that can assume this role is the GitHub Open ID provider.
+     *
+     * This role is granted authority to assume aws cdk roles; which are created
+     * by the aws cdk v2.
+     */
+    new iam.Role(this, "GitHubActionsRole", {
+      assumedBy: GitHubPrincipal,
+      description:
+        "Role assumed by GitHubPrincipal for deploying from CI using aws cdk",
+      roleName: "github-ci-role",
+      maxSessionDuration: cdk.Duration.hours(1),
+      inlinePolicies: {
+        CdkDeploymentPolicy: new iam.PolicyDocument({
+          assignSids: true,
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ["sts:AssumeRole"],
+              resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
+            }),
+          ],
+        }),
+      },
+    });
+
+    new ec2.Vpc(this, "GrowthDay2022AwsJamesMoughonVpc", {
+      // CHANGE: this is where we define how many AZs to use
+      maxAzs: 2,
+
+      // CHANGE: We define a single subnet configuration per AZ.
+      subnetConfiguration: [
+        {
+          // CHANGE: this is it's CIDR mask so 255.255.255.0
+          cidrMask: 24,
+
+          // CHANGE: a name for each of these subnets
+          name: "GrowthDay2022AwsJamesMoughon-subnet",
+
+          // CHANGE: and the subnet type to be used - here we will have
+          // a public subnet. There are other options available here.
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
